6151087144da35a0718cf5576469adb48d922651
User <User@LAPTOP-L9CAK3BK>
2018-09-23 18:14:47 +0800

    shapes

---
+++ WIX1002/src/wix1002/W01L03.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wix1002;
+
+/**
+ *
+ * @author User
+ */
+public class W01L03 
+{
+    public static void main(String[] args)
+    {
+      System.out.println("*********       ****         *           *");  
+      System.out.println("*       *      *    *       ***         * *");  
+      System.out.println("*       *     *      *     *****       *   *");  
+      System.out.println("*       *     *      *       *        *     *");  
+      System.out.println("*       *     *      *       *       *       *");  
+      System.out.println("*       *     *      *       *        *     *")  ;
+      System.out.println("*       *     *      *       *         *   *")  ;
+      System.out.println("*       *      *    *        *          * *")  ;
+      System.out.println("*********       ****         *           *")  ;
+    }
+}
